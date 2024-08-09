5226466bda76a5f0aacf7aa31d56ebe0551e3fbb
kalebhas <kaleb.hasselstrom@gmail.com>
2024-08-09 14:54:13 -0600

    Database now a class, Ammo added as method

--- Database.py
+++ Database.py
@@ -1,24 +1,25 @@
 import mysql.connector
 
-# Establish a connection
-connection = mysql.connector.connect(
-    host='69.48.178.203',
-    user='qttnet_2407Blue_User',
-    password='d00edjuhme',
-    database='qttnet_2407Blue'
-)
+class Ammo:
+    def __init__(self, AmmoType = ""):
+        self.AmmoType = AmmoType
+    
+    def cartidge(self, AmmoType):
+        connection = mysql.connector.connect(
+            host='69.48.178.203',
+            user='qttnet_2407Blue_User',
+            password='d00edjuhme',
+            database='qttnet_2407Blue'
+        )
+        # Create a cursor object to interact with the database
+        cursor = connection.cursor()
+        query = "SELECT * FROM `Ammunition` WHERE `Cartridge` LIKE " + AmmoType
+        # Execute a query
+        cursor.execute(query)
+        # Fetch the results
+        results = cursor.fetchall()
+        # Close the connection
+        connection.close()
+        
+        return results
 
-# Create a cursor object to interact with the database
-cursor = connection.cursor()
-
-# Execute a query
-cursor.execute("SELECT * FROM Ammunition")
-
-# Fetch the results
-results = cursor.fetchall()
-for row in results:
-    print(row)
-
-# Close the connection
-connection.close()
-#4.4.146.42
---
+++ test.py
@@ -0,0 +1,6 @@
+from Database import Ammo
+
+
+search = "'.32 Winchester Special'"
+result = Ammo()
+print(result.cartidge(search))
