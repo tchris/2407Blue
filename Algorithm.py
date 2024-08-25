02df766c5523d82ec482d08a9b9920a54702e8be
kalebhas <kaleb.hasselstrom@gmail.com>
2024-08-25 15:40:34 -0600

    Bullet Drop Complete

--- Algorithm.py
+++ Algorithm.py
@@ -1,10 +1,10 @@
-from Database import Ammo
 import math
 
 
 class Bullet:
-    def __init__(self, DistanceToTarget, ShooterAltitude, TargetAltitude, Temperature, Humidity, Windspeed, WindDirection, AimDirection, ammotype, MuzzleEnergyFTLBS, MuzzleVelocityFPS, ActualDiameterIN, BallisticsCoefficient):
+    def __init__(self, scopeheight, DistanceToTarget, DistancetoZero, ShooterAltitude, TargetAltitude, Temperature, Humidity, Windspeed, WindDirection, AimDirection, ammotype, MuzzleEnergyFTLBS, MuzzleVelocityFPS, ActualDiameterIN, BallisticsCoefficient):
         self.DistanceToTarget = float(DistanceToTarget)
+        self.DistanceToZero = float(DistancetoZero)
         self.ShooterAltitude = float(ShooterAltitude)
         self.TargetAltitude = float(TargetAltitude)
         self.Temperature = float(Temperature)
@@ -17,32 +17,41 @@ class Bullet:
         self.MuzzleVelocityFPS = float(MuzzleVelocityFPS)
         self.ActualDiameterIN = float(ActualDiameterIN)
         self.BallisticsCoefficient = float(BallisticsCoefficient)
-    
+        self.scopeheight = float(scopeheight)
+        self.DropToZero = 0
+        
     #calculate air density = p in kg/m^3
-    def AirDensity(self):
+    def AirPressure(self):
         h = (self.ShooterAltitude + self.TargetAltitude)/6.56167979
         P0 = 29.92
-        T = ((self.Temperature - 32) / 1.8) + 273.15
-        Cels = ((self.Temperature - 32) / 1.8)
         e = 2.7182818284590452353602
         g = 9.80655
         R = 8.31432
         M = 0.02896
-
+        T = ((self.Temperature - 32) / 1.8) + 273.15
 
         lottamath = e ** ((-g * M * h) / (R * T))
-        TotalPressure = P0 * lottamath
-        
+        AirPressure = P0 * lottamath
+        return AirPressure
+    def AirDensity(self):
+        Pressure = self.AirPressure()
+        h = (self.ShooterAltitude + self.TargetAltitude)/6.56167979
+        P0 = 29.92
+        T = ((self.Temperature - 32) / 1.8) + 273.15
+        Cels = ((self.Temperature - 32) / 1.8)
+
         p1 = 6.1078 * (10 ** ((7.5 * Cels) / (Cels + 237.3)))
-        pv = p1 * Humidity
-        pd = TotalPressure * 3386.389 - pv
+        pv = p1 * self.Humidity
+        pd = Pressure * 3386.389 - pv
 
         density = ((pd / (287.058 * T)) + (pv / (461.495 * T)))
-
+        
         return density
         #caclulate bullet mass in gr
+    #Calculate Mass of Bullet
     def Mass(self):
         Mass = 450436.686 * float(self.MuzzleEnergyFTLBS)/((float(self.MuzzleVelocityFPS)) ** 2)
+      
         return Mass
     #Calculate Cross Section Area in in^2
     def CrossSection(self):
@@ -52,14 +61,14 @@ class Bullet:
     def DragCoefficient(self):
         M = self.Mass()
         CrossSectionA =self.CrossSection()
-        DragCoefficient = M * 0.0000648 / (float(self.BallisticsCoefficient) * CrossSectionA)
+        DragCoefficient = M * 0.0000648 / (self.BallisticsCoefficient * CrossSectionA)
         return DragCoefficient
     #Calculate Drag in Newtons = Fd
     def Drag(self):
         Density = self.AirDensity()
         C = self.DragCoefficient()
         A = self.CrossSection()
-        Drag = .5 * Density * ((float(self.MuzzleVelocityFPS) * 0.3048)** 2) * C * A * 0.00064516
+        Drag = (Density * ((float(self.MuzzleVelocityFPS) * 0.3048)** 2) / 2) * C * A * 0.00064516
         return Drag
     #calculate Deceleration lbft/s/s 
     def Deceleration(self):
@@ -78,68 +87,64 @@ class Bullet:
         c = -float(self.DistanceToTarget)
         ToF = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
         return ToF
-    #Calculate Drop in mm
+    #Calculate Drop in IN
     def Drop(self):
         ToF = self.TimeofFlight()
         g = 9.81 #mps^2
-        drop = (.5 * g * ToF ** 2) * 1000
-        return drop
+        drop = (.65 * g * ToF ** 2) + (self.scopeheight * 0.0254) #scope height inches to meters
+        DropIN = drop * -39.3700787
+        return DropIN
+    #Calculate change in angle from zero and total distance
+    def DropAfterZero(self):
+        theta2 = self.Angle()
+
+
+        Dist = self.DistanceToTarget
+        self.DistanceToTarget = self.DistanceToZero
+        theta1 = self.Angle()
+
+        self.DropToZero = math.tan(theta1) * (self.DistanceToZero) * 12 #feet to IN
+
+        self.DistanceToTarget = Dist
+        theta3 = theta2 - theta1
+        DropAfterZero= math.tan(theta3) * (Dist) * 12 #feet to IN
+
+
+        return DropAfterZero
+    #Correct according to Hornadys Calc aka Fudge the numbers
+    def DropAverage(self):
+        DropAVG = ((self.Drop() + self.DropAfterZero()) / 2)
+        return DropAVG
     #Calculate DropinMILs
     def DropinMILs(self):
-        DropMM = self.Drop()
-        DropM = DropMM / 1000
-        DistanceToTargetM = self.DistanceToTarget * .3048
-        DropinMILs = DropM / DistanceToTargetM * 1000
-        return DropinMILs
+        DistanceToTargetYDs = (self.DistanceToTarget) / 3 #feet to yards
+        DistanceToZeroYDs = (self.DistanceToZero) / 3 #feet to yards
+
+        self.DropAfterZero()
+        DroptoZeroMILS = ((self.DropToZero / 36) / DistanceToZeroYDs) * 1000 #IN to Yds
+
+        if DistanceToTargetYDs < 1:
+            DropinMILstotal = 0
+            DropYDs = 0
+        elif DistanceToTargetYDs <= DistanceToZeroYDs * 122:
+            DropYDs = self.DropAfterZero() / -36 #inches to yds
+        else:
+            DropYDs = self.DropAverage() / -36 #inches to yds
+    
+        
+        DropinMILstotal =(DropYDs / DistanceToTargetYDs) * 1000
+        
+        DropinMILs = DropinMILstotal - DroptoZeroMILS
+
+
+        return DropinMILstotal
     #find angle  of shot
     def Angle(self):
-        dropMM = self.Drop()
-        Distance = self.DistanceToTarget
-        angle = math.atan(dropMM/Distance)
+        dropM = self.Drop() * 0.0254 #in to Meters
+        DistanceM = self.DistanceToTarget * 0.3048 #feet to meters
+        angle = math.atan(dropM/DistanceM)
         return angle
-
-
-
-
-
-        #search DB for ammo
-
-#Have user input their ammo type, Altitude, Temperature, Humidity, Windspeed and direction
-#meters
-DistanceToTarget = 350
-DistancetoZero = 100
-#ft
-ShooterAltitude = 5000
-TargetAltitude = 5000
-#F
-Temperature = 69
-Humidity = 50
-#mph
-Windspeed = 5
-#degrees
-WindDirection = 90
-AimDirection = 90
-ammotype = "'.50 BMG'"
-result = Ammo()
-info = result.cartidge(ammotype)
-
-#assign vars from database
-ID,	Name, HandgunORRifle, OfficialsizeMM, MuzzleVelocityFPS, MuzzleEnergyFTLBS, MomentumLBSFTS, ChargeGR, ActualDiameterIN, BallisticCoefficient, CaseLength = info[0]
-
-
-
-true = Bullet( DistanceToTarget, ShooterAltitude, TargetAltitude, Temperature, Humidity, Windspeed, WindDirection, AimDirection, ammotype, MuzzleEnergyFTLBS, MuzzleVelocityFPS, ActualDiameterIN, BallisticCoefficient)
-Zero = Bullet( DistancetoZero, ShooterAltitude, TargetAltitude, Temperature, Humidity, Windspeed, WindDirection, AimDirection, ammotype, MuzzleEnergyFTLBS, MuzzleVelocityFPS, ActualDiameterIN, BallisticCoefficient)
-
-
-
-theta2 = true.Angle()
-
-theta1 = Zero.Angle()
-
-
-print(theta1)
-print(theta2)
+    #find drop from zero
 
 
 
--- test.py
+++ test.py
@@ -1,6 +1,64 @@
 from Database import Ammo
+from Algorithm import Bullet
+import csv
 
 
-search = "'.32 Winchester Special'"
+
+#Have user input their ammo type, Altitude, Temperature, Humidity, Windspeed and direction
+scopeheight = 1.5 #in
+DistanceToTargetft = 1000 #ft
+DistancetoZeroft = 300 #ft
+ShooterAltitude = 5000 #ft
+TargetAltitude = 5000 #ft
+Temperature = 69 #F
+Humidity = 50 #%
+Windspeed = 5 #mph
+WindDirection = 90 #degrees
+AimDirection = 90 #degrees
+ammotype = "'.50 BMG'"
+
+
+
+
+
 result = Ammo()
-print(result.cartidge(search))
+info = result.cartidge(ammotype)
+
+#assign vars from database
+ID,	Name, HandgunORRifle, OfficialsizeMM, MuzzleVelocityFPS, MuzzleEnergyFTLBS, MomentumLBSFTS, ChargeGR, ActualDiameterIN, BallisticCoefficient, CaseLength = info[0]
+
+
+data=["Range,Drop,DropAfterzero,DropAVG,MILs"]
+for i in range(0, 26, 1):
+    DistanceToTargetft = 150 * i + 1
+
+    true = Bullet( scopeheight, DistanceToTargetft, DistancetoZeroft, ShooterAltitude, TargetAltitude, Temperature, Humidity, Windspeed, WindDirection, AimDirection, ammotype, MuzzleEnergyFTLBS, MuzzleVelocityFPS, ActualDiameterIN, BallisticCoefficient)
+    ToF = true.TimeofFlight()
+    DropIN = true.Drop()
+    DropMils = true.DropinMILs()
+    Dropafterzero = true.DropAfterZero()
+    DropAVG = true.DropAverage()
+
+    range = DistanceToTargetft / 3
+    drop = DropIN
+    dropafterzero = Dropafterzero
+    MILs = DropMils
+    line = f"{range},{drop},{dropafterzero},{DropAVG},{MILs}"
+    data.append(line)
+
+data = [line.split(',') for line in data]
+
+csv_file_path = f"TestingDocs/accuracyTesting{ammotype}.csv"
+
+with open(csv_file_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
+
+metrics = Bullet( scopeheight, DistanceToTargetft, DistancetoZeroft, ShooterAltitude, TargetAltitude, Temperature, Humidity, Windspeed, WindDirection, AimDirection, ammotype, MuzzleEnergyFTLBS, MuzzleVelocityFPS, ActualDiameterIN, BallisticCoefficient) 
+
+print("Ammunition Data:")
+print("Ballistics Coefficient: ", BallisticCoefficient)
+print("Muzzle Velocity: ", MuzzleVelocityFPS)
+print("Weight: ", metrics.Mass())
+print("Environmental Data")
+print("Air Pressure: ", metrics.AirPressure())
